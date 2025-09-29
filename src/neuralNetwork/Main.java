6f2de5d2d2dc4f83ac0f942c55b2f5bfc8542ea8
Davide-scolp <scolpatidavide@gmail.com>
2025-09-29 19:20:11 +0200

    Rename classes and folders

--- src/neuralNetwork/Main.java
+++ src/neuralNetwork/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package davidescolpatireteneurale;
+package neuralNetwork;
 
 /**
  *
- * @author studente
+ * @author Davide_Scolpati
  */
-public class DavideScolpatiReteNeurale {
+public class Main {
 
     /**
      * @param args the command line arguments
